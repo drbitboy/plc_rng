15938b81e169a737318d6cf260029d8fd6279289
Brian Carcich <BrianTCarcich@gmail.com>
2019-12-31 04:26:34 -0500

    Rename variables in verify/ln_check.f to match NASA document

--- verify/ln_check.f
+++ verify/ln_check.f
@@ -10,10 +10,10 @@ C      NASLN  REAL*4; output; of NASA polynomial approximation
 C      TAYLN  REAL*4; output; of Taylor series approximation
 C   ERRLN(2)  REAL*4; output; errors wrt TRULN of NAS/TAYLN
 C
-      subroutine ln_check(x,xshift,truln,nasln,tayln,errln)
+      subroutine ln_check(x,yshift,truln,nasln,tayln,errln)
       implicitnone
       real*4 x
-      real*4 xshift
+      real*4 yshift
       real*4 truln
       real*4 nasln
       real*4 tayln
@@ -26,13 +26,13 @@ C     Cf. https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19720010958.pdf
 
 C     Intermediate values
       integer*4 i
-      real*4 y
-      real*4 y2
+      real*4 v
+      real*4 v2
       real*4 taytrm
       real*4 ln2
       real*4 sqrt2
       real*4 nln2
-      real*4 xshiftlim
+      real*4 yshiftlim
 
 CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 
@@ -42,39 +42,39 @@ C     Use intrinsic function as "truth"
       ln2 = log(2.0)
       sqrt2 = sqrt(2.0)
       nln2 = 0.0
-      xshift = x
+      yshift = x
 
-      xshiftlim = sqrt2 / 256.0
-      if (xshift.lt.xshiftlim) then
-        xshift = xshift * 256.0
+      yshiftlim = sqrt2 / 256.0
+      if (yshift.lt.yshiftlim) then
+        yshift = yshift * 256.0
         nln2 = nln2 + (8.0 * ln2)
       endif
 
-      xshiftlim = sqrt2 / 16.0
-      if (xshift.lt.xshiftlim) then
-        xshift = xshift * 16.0
+      yshiftlim = sqrt2 / 16.0
+      if (yshift.lt.yshiftlim) then
+        yshift = yshift * 16.0
         nln2 = nln2 + (4.0 * ln2)
       endif
 
-      xshiftlim = sqrt2 / 4.0
-      if (xshift.lt.xshiftlim) then
-        xshift = xshift * 4.0
+      yshiftlim = sqrt2 / 4.0
+      if (yshift.lt.yshiftlim) then
+        yshift = yshift * 4.0
         nln2 = nln2 + (2.0 * ln2)
       endif
 
-      xshiftlim = sqrt2 / 2.0
-      if (xshift.lt.xshiftlim) then
-        xshift = xshift * 2.0
+      yshiftlim = sqrt2 / 2.0
+      if (yshift.lt.yshiftlim) then
+        yshift = yshift * 2.0
         nln2 = nln2 + (1.0 * ln2)
       endif
 
 C     Converted argument, and argument squared
-      y = (xshift - 1) / (xshift + 1)
-      y2 = y*y
+      v = (yshift - 1) / (yshift + 1)
+      v2 = v*v
 
 C     Chebyshev-based polynomial
 
-      nasln = (y * (2 + (y2 / (q00 + (q01 * y2)))) - nln2)
+      nasln = (v * (2 + (v2 / (q00 + (q01 * v2)))) - nln2)
       errln(1) = nasln - truln
 
 C     Taylor series
@@ -82,7 +82,7 @@ C     Taylor series
       tayln = -nln2
       taytrm = -1.0
       do i=5,1,-1
-        taytrm = ((xshift - 1.0) ** i) / i
+        taytrm = ((yshift - 1.0) ** i) / i
         if (mod(i,2).eq.0) taytrm = -taytrm
         tayln = tayln + taytrm
       enddo
@@ -105,7 +105,7 @@ C [factor] is the factor per step
       integer i
       real*4 factor
       real*4 x
-      real*4 xshift
+      real*4 yshift
 
 CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 
@@ -120,17 +120,17 @@ C     - Print results
       x = sqrt(2.0) / 2.0
       do i=-20,20,1
         if (i.eq.0) x = 1.0
-        call ln_check(x,xshift,truln,nasln,tayln,errln)
-        print*,i,x,xshift,truln,nasln,tayln,errln
+        call ln_check(x,yshift,truln,nasln,tayln,errln)
+        print*,i,x,yshift,truln,nasln,tayln,errln
         x = x * factor
       enddo
 
       x = 1.0 / 32768.0
       do i=1,16
-        call ln_check(x,xshift,truln,nasln,tayln,errln)
-        print*,i,x,xshift,truln,nasln,tayln,errln
-        call ln_check(sqrt(2.0)*x,xshift,truln,nasln,tayln,errln)
-        print*,i,sqrt(2.0)*x,xshift,truln,nasln,tayln,errln
+        call ln_check(x,yshift,truln,nasln,tayln,errln)
+        print*,i,x,yshift,truln,nasln,tayln,errln
+        call ln_check(sqrt(2.0)*x,yshift,truln,nasln,tayln,errln)
+        print*,i,sqrt(2.0)*x,yshift,truln,nasln,tayln,errln
         x = x * 2.0
       enddo
       end
