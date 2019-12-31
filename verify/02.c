c9df5bea28b8d1427e584a6f589a006e7984549c
Brian Carcich <BrianTCarcich@gmail.com>
2019-12-30 20:28:55 -0500

    Added comments to verify/02.c

--- verify/02.c
+++ verify/02.c
@@ -1,20 +1,37 @@
-/* Usage:  ./02 [20077[ 12345]] */
+/***********************************************************************
+
+Purpose:
+
+  Verify that 16-bit LCG (a,b) pair does
+  not repeat in less than 32,768 steps
+
+
+Usage:
+
+  ./02 [20077[ 12345]]
+
+***********************************************************************/
 
 #include <math.h>
 #include <stdio.h>
 
 int
 main(int argc, char** argv) {
-short a = 20077;
-short b = 12345;
-short lcg;
-short bits[2048];
-short iword;
-int icount;
+short a = 20077;          /* Default a of LCG */
+short b = 12345;          /* Default b of LCG */
+short seed;               /* Linear Congruential Generator seed */
+short bits[2048];         /* Store 32,768 bits to look for duplicates */
+short iword;              /* Index into bits array */
+int isteps;               /* Step counter */
+
+  /* Parse command line */
 
   if (1<argc) {
   int n;
   int itmp = -1;
+
+    /* - Value for a should be present */
+
     n = sscanf(argv[1], "%d", &itmp);
     if (n!=1 || itmp<0 || itmp > 32767) {
       fprintf(stderr, "Invalid value for a; a=%d; n=%d\n", a, n);
@@ -23,7 +40,11 @@ int icount;
       a = (short)itmp;
     }
 
-    if (1<argc) {
+    if (2<argc) {
+
+      /* - Value for b should be present */
+
+      itmp = -1;
       n = sscanf(argv[2], "%d", &itmp);
       if (n!=1 || itmp<0 || itmp > 32767) {
         fprintf(stderr, "Invalid value for b; a=%d; n=%d\n", b, n);
@@ -35,24 +56,46 @@ int icount;
   }
   printf("(%d,%d)=(a,b)\n",(int)a,(int)b);
 
-  for (iword=0; iword<2048; ++iword) bits[iword] = 0;
+  /* Clear all bits, initialize counter and seed */
 
-  icount = 0;
-  lcg = 0;
+  for (iword=0; iword<2048; ++iword) bits[iword] = 0;
+  isteps = 0;
+  seed = 0;
+
+  /* Loop until duplicate LCG seed is found */
 
   while (1) {
   short ibit;
-    iword = lcg >> ((short)4);
-    ibit = 1 << (lcg & ((short)0x0f));
+
+    /* seed => index into bits[] array and bit in bits[iword] */
+
+    iword = seed >> ((short)4);
+    ibit = 1 << (seed & ((short)0x0f));
+
+    /* Check if bit for seed has been set, indicating that this seed
+       is the first duplicate:  if it is, exit the loop
+     */
+
     if ((bits[iword] & ibit)) break;
 
+    /* If it is not a duplicate, set that bit for future checks and
+       increment isteps
+     */
     bits[iword] |= ibit;
-    ++icount;
+    ++isteps;
 
-    lcg = ((lcg * a) + b) & 0x07fff;
-  }
+    /* Calculate next seed using LCG algorithm */
 
-  printf("Found duplicate LCG value = %d after %d steps\n", (int)lcg, icount);
+    seed = ((seed * a) + b) & 0x07fff;
 
-  return 0;
+  } /* End of [while (1)] loop */
+
+  /* Output result */
+
+  printf("Found duplicate LCG seed [%d] after %d steps; %s\n"
+        ,(int)seed, isteps
+        ,32768==isteps ? "success" : "failure"
+        );
+
+  return 32768==isteps ? 0 : -1;
 }
